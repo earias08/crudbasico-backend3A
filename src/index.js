3d62a22d6184572775ab16b6bfa1a095d0661f13
Emilse <emilsearias@gmail.com>
2021-05-03 18:33:34 -0300

    se agrego la rama configuraciones para agregar el puerto usando un archivo .env

--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
 import express from 'express';
 
+// setup
 // creo una instancia de express
 const app = express();
 
 const port = 4000;
+// crear el puerto
 
 app.listen(port, ()=>{
     console.log('Estoy usando el puerto '+port)
 });
+
+//herramientras extras
+
+
+// rutas
 
